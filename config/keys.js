28c122ceb212ad9427566693bef4729c9a555260
Rohan Keskar <33134458+rohankeskar19@users.noreply.github.com>
2019-07-29 01:37:28 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoDBUri: "mongodb://localhost/minsocial",
+  mongoDBUri: "mongodb://rohan:Totalfire12345$@ds113482.mlab.com:13482/min-social-media",
   saltRounds: 10,
   secret: "ahsfk1924usnfjkhafs194y!#$!@39r214nsf",
   tokenDuration: "1h",
