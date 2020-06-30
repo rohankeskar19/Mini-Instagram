92af3e5969beb1691a9a9b56764512fbbb8925ad
Rohan Keskar <33134458+rohankeskar19@users.noreply.github.com>
2020-06-30 15:47:07 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,9 @@
 module.exports = {
   mongoDBUri:
-    "mongodb+srv://rohankeskar:Totalfire12345$@cluster0-051ly.mongodb.net/test?retryWrites=true&w=majority",
+    "mongouri",
   saltRounds: 10,
   secret: "ahsfk1924usnfjkhafs194y!#$!@39r214nsf",
   tokenDuration: "1h",
-  accesKey: "AKIAVLCAOPF57L6Z7AX2",
-  secretAccessKey: "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"
+  accesKey: "awskey",
+  secretAccessKey: "awskey"
 };
