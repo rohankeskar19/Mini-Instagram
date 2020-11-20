926670fd8bebded9dcc08e7de7de1d5404e3935c
Rohan Keskar <33134458+rohankeskar19@users.noreply.github.com>
2020-11-20 17:04:24 +0530

    Delete

--- config/production.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoDBUri: process.env.mongoDBUri,
-  saltRounds: 10,
-  secret: process.env.secret,
-  tokenDuration: "5h",
-  accesKey: process.env.accesKey,
-  secretAccessKey: process.env.secretAccessKey
-};
