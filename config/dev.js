88c408c0039d82fa8859113587922ee62061f724
Rohan Keskar <33134458+rohankeskar19@users.noreply.github.com>
2020-11-20 17:04:38 +0530

    Delete

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoDBUri:
-    "mongouri",
-  saltRounds: 10,
-  secret: "ahsfk1924usnfjkhafs194y!#$!@39r214nsf",
-  tokenDuration: "1h",
-  accesKey: "awskey",
-  secretAccessKey: "awskey"
-};
